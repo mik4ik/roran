5cc5e2cd161d249a13a3a0e2aa6865c5290c3740
mik4ik <mik4ik@gmail.com>
2017-10-07 02:26:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# How to run
+---
+##### 1. Install node.js and npm
+[https://www.npmjs.com/get-npm](https://www.npmjs.com/get-npm)
+
+##### 2.  Clone or download project.
+
+##### 3. Install gulp
+```npm i gulp -g```
+[https://gulpjs.com/](https://gulpjs.com/)
+
+##### 4. Go to project root and install node_modules
+```npm i```
+
+##### 5. In project root run gulp
+```gulp```
+
+---
+##### You can look at ready result [here](http://roran.mik4ik.pro/)
+
+##### You can look at [google PageSpeed Insights](https://developers.google.com/speed/pagespeed/insights/?hl=ru&url=http%3A%2F%2Froran.mik4ik.pro%2F&tab=mobile) result
+
+##### You can look at [responsinator.com](http://www.responsinator.com/?url=roran.mik4ik.pro%2F)
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var	gulp			=	require('gulp'),
+    browserSync		=	require('browser-sync'),
+    notify			=	require("gulp-notify"),
+    del             =   require('del'),
+    sass			=	require('gulp-sass'),
+    rename			=	require('gulp-rename'),
+    autoprefixer	=	require('gulp-autoprefixer'),
+    cleanCSS		=	require('gulp-clean-css'),
+    concat			=	require('gulp-concat'),
+    uglify			=	require('gulp-uglify'),
+    spritesmith		=	require('gulp.spritesmith');
+
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {baseDir: 'app'},
+        notify: false
+    });
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/*.sass')
+        .pipe(sass().on("error", notify.onError()))
+        .pipe(rename({suffix: '.min', prefix : ''}))
+        .pipe(autoprefixer({browsers: ['last 30 versions']}))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/lightslider/dist/js/lightslider.min.js',
+        'bower_components/mixitup/build/jquery.mixitup.min.js',
+        'app/js/common.js'
+    ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('watch', ['sass', 'scripts', 'browser-sync'], function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/common.js',['scripts'], browserSync.reload);
+});
+
+gulp.task('sprite', function() {
+    var spriteData = gulp.src('app/img/icons/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_sprite.sass',
+            imgPath: '../img/sprite.png',
+            cssFormat: 'sass',
+            padding: 5
+        }));
+    var imgStream = spriteData.img.pipe(gulp.dest('app/img/'));
+    var cssStream = spriteData.css.pipe(gulp.dest('app/sass/basis/'));
+    return (imgStream, cssStream);
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('build', ['clean', 'sass', 'scripts'], function() {
+
+    gulp.src(['app/css/styles.min.css'])
+        .pipe(gulp.dest('dist/css'));
+
+    gulp.src('app/img/**/*')
+        .pipe(gulp.dest('dist/img'));
+
+    gulp.src('app/js/**/*')
+        .pipe(gulp.dest('dist/js'));
+
+    gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'));
+
+});
